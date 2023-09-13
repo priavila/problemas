eaa7ad8228345af9c2206253f1725c2a8857f3d0
Priscylla Ávila <140451660+priavila@users.noreply.github.com>
2023-09-13 16:32:56 -0300

    new exercicio 1043

---
+++ Triangulo.java
@@ -0,0 +1,24 @@
+package beecrowd.problemas;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Triangulo {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in).useLocale(Locale.US);
+
+        double A = input.nextDouble();
+        double B = input.nextDouble();
+        double C = input.nextDouble();
+
+        if(A < B + C && B < A + C && C < A + B) {
+            double perimetro = A + B + C;
+            System.out.printf("Perimetro = %.1f%n", perimetro);
+        }else {
+            double area = ((A + B) * C) / 2;
+            System.out.printf("Area = %.1f%n", area);
+        }
+
+        input.close();
+    }
+}
