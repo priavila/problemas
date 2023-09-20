596fc2f4b5eb1ba1aad04017a601bb5646717474
Priscylla Ávila <140451660+priavila@users.noreply.github.com>
2023-09-20 14:25:36 -0300

    new problem 1064

---
+++ nivel1/PositivosEMedia.java
@@ -0,0 +1,35 @@
+package beecrowd.problemas.nivel1;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class PositivosEMedia {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in).useLocale(Locale.US);
+
+        double valor;
+        int contador = 0;
+        double soma = 0;
+        double media = 0;
+        int cc = 0;
+
+        while(contador < 6 ) {
+            valor = input.nextDouble();
+            contador++;
+
+            if(valor > 0) {
+                cc++;
+            }
+
+            if (valor > 0) {
+                soma = soma + valor;
+                media = soma / cc;
+            }
+        }
+
+        System.out.printf("%d valores positivos%n", cc);
+        System.out.printf("%.1f%n", media);
+
+        input.close();
+    }
+}
