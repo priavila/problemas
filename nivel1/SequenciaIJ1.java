e5be9ece8bdd4e6ac0314ebc910e9f18aee45da6
Priscylla Ávila <140451660+priavila@users.noreply.github.com>
2023-10-16 18:09:01 -0300

    new problem 1095

---
+++ nivel1/SequenciaIJ1.java
@@ -0,0 +1,11 @@
+package beecrowd.problemas.nivel1;
+
+public class SequenciaIJ1 {
+    public static void main(String[] args) {
+
+        for(int I=1, J=60; J>=0; I+=3, J-=5) {
+            System.out.println("I" + "=" + I + " " + " J" + "=" + J);
+        }
+
+    }
+}
